0d4cb293b48e3b2cb32f19fac3038393040da6eb
anko94 <raion777>
2018-04-09 02:11:59 +0300

    added new options

--- main.py
+++ main.py
@@ -4,39 +4,105 @@ from data.source import Source
 from solver import Solver
 from visualization import Visualization
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def drawTask():
+def drawTask(arcCenter, arcAngle, arcRadius, dielectricLowerLeftPoint, dielectricWidth, dielectricHeight, sourcePoint):
     # all angles in degrees
     # lower left point, width, height, rotateAngle
-    dielectricRectangle = DielectricRectangle([0, 0], 1, 2, -90)
+    dielectricRectangle = DielectricRectangle(dielectricLowerLeftPoint, dielectricWidth, dielectricHeight, -90)
     # point
-    source = Source([2, 2])
+    source = Source(sourcePoint)
     # center, radius, angle, rotateAngle
-    arc = Arc([-2, -2], 1, 30, -90)
+    arc = Arc(arcCenter, arcRadius, arcAngle*180/math.pi, -90)
     # task draw period, arc draw period, rectangle's points draw period, dielectric draw period
     visualization = Visualization(0.1, 0.1, 0.1, 0.1)
     visualization.drawTask(dielectricRectangle, source, arc)
-    visualization.fillArc(arc, 10)
+    visualization.fillArc(arc, 40)
     visualization.fillDielectricRectangle(dielectricRectangle, 10, 10)
     visualization.blockPlot()
 
 
+def plotFieldStrengthByAngle(arcAngle, PHI1, PHI2, PHI3):
+    ax = plt.subplot(111)
+    alpha1 = np.linspace(-arcAngle, arcAngle, len(PHI1))
+    alpha2 = np.linspace(-arcAngle, arcAngle, len(PHI2))
+    alpha3 = np.linspace(-arcAngle, arcAngle, len(PHI3))
+    plt.plot(alpha1, PHI1, label="n=%d" % (len(PHI1),))
+    plt.plot(alpha2, PHI2, label="n=%d" % (len(PHI2),))
+    plt.plot(alpha3, PHI3, label="n=%d" % (len(PHI3),))
+
+    leg = plt.legend(loc='best', ncol=1, mode="expand", shadow=True, fancybox=True)
+    leg.get_frame().set_alpha(0.5)
+
+    plt.show()
+
+
+def listAbs(list):
+    absList = []
+    for i in range(len(list)):
+        absList.append(abs(list[i]))
+    return absList
+
+
+def norm(list, v):
+    for i in range(len(list)):
+        list[i] = list[i]/v
+    return list
+
+
 if __name__ == "__main__":
-    solver = Solver()
+    arcCenter = [1, -1]
+    arcAngle = 60 * math.pi/180          #angle in radians
+    arcRadius = 1
+    dielectricLowerLeftPoint = [0.5, -0.5]
+    dielectricWidth = 0.4
+    dielectricHeight = 1
+    sourcePoint = [1, 1]
+
+    nArcPoints = 10
+    nDielectricWidthPoints = 7
+    nDielectricHeightPoints = 7
+
+    solver = Solver(2*math.pi,                     #k0  - волновое число вне диэлектрика
+                    2*math.pi*1.000001,                     #k1  - волновое число внутри диэлектрика
+                    1,                     #I   - амплитуда силы тока
+                    8.854188*10**(-12),    #e0  - диэлектрическая проницаемость
+                    4*math.pi*10**(-7))    #nu0 - магнитная проницаемость
+
     dielectricRectangle = DielectricRectangle([0, 0], 1, 2, -90)
-    arc = Arc([-2, -2], 1, 30, -90)
-    result = solver.solve(100, 10, dielectricRectangle.breakUpRectangleByNMPoints(10, 10), arc.putVerticesInCorrectOrder(arc.breakUpArcByNPoints(10)))
-    U = result[0]
-    PHI = result[1]
-    result = solver.solve(200, 20, dielectricRectangle.breakUpRectangleByNMPoints(20, 10),
-                          arc.putVerticesInCorrectOrder(arc.breakUpArcByNPoints(20)))
-    U1 = result[0]
+    arc = Arc([1, -1], 1, 30, -90)
+    source = Source([1, 1])
+    result = solver.solve(nDielectricWidthPoints*nDielectricHeightPoints, nArcPoints,
+                          dielectricRectangle.breakUpRectangleByNMPoints(nDielectricWidthPoints, nDielectricHeightPoints),
+                          arc.putVerticesInCorrectOrder(arc.breakUpArcByNPoints(nArcPoints)), source)
     PHI1 = result[1]
-    result = solver.solve(400, 40, dielectricRectangle.breakUpRectangleByNMPoints(40, 10),
-                          arc.putVerticesInCorrectOrder(arc.breakUpArcByNPoints(40)))
-    U2 = result[0]
+
+    nArcPoints = 20
+    nDielectricWidthPoints = 7
+    nDielectricHeightPoints = 7
+
+    result = solver.solve(nDielectricWidthPoints * nDielectricHeightPoints, nArcPoints,
+                          dielectricRectangle.breakUpRectangleByNMPoints(nDielectricWidthPoints,
+                                                                         nDielectricHeightPoints),
+                          arc.putVerticesInCorrectOrder(arc.breakUpArcByNPoints(nArcPoints)), source)
     PHI2 = result[1]
-    print(PHI[0], PHI1[0], PHI2[0])
-    print(U[0], U1[0], U2[0])
-    # drawTask()
+
+    nArcPoints = 40
+    nDielectricWidthPoints = 7
+    nDielectricHeightPoints = 7
+
+    result = solver.solve(nDielectricWidthPoints * nDielectricHeightPoints, nArcPoints,
+                          dielectricRectangle.breakUpRectangleByNMPoints(nDielectricWidthPoints,
+                                                                         nDielectricHeightPoints),
+                          arc.putVerticesInCorrectOrder(arc.breakUpArcByNPoints(nArcPoints)), source)
+    PHI3 = result[1]
+
+    absPHI1 = listAbs(PHI1)
+    absPHI2 = listAbs(PHI2)
+    absPHI3 = listAbs(PHI3)
+    maxV = max([max(absPHI1), max(absPHI2), max(absPHI3)])
+
+    plotFieldStrengthByAngle(arcAngle, norm(absPHI1, maxV), norm(absPHI2, maxV), norm(absPHI3, maxV))
+    # drawTask(arcCenter, arcAngle, arcRadius, dielectricLowerLeftPoint, dielectricWidth, dielectricHeight, sourcePoint)
--- solver.py
+++ solver.py
@@ -5,52 +5,52 @@ import numpy as np
 
 class Solver:
 
+    def getInitialFieldStrength(self, e, nu, k, P, P0):
+        r = math.sqrt((P0[0] - P[0]) ** 2 + (P0[1] - P[1]) ** 2)
+        r = r if r != 0 else self.accuracy
+        return -k*self.I*math.sqrt(nu)/(4*math.sqrt(e)) * mpmath.hankel1(0, k * r)
+
     def G(self, k, M, P):
-        r = math.sqrt((M[0] - P[0])**2 + (M[1] - P[1])**2) if \
-            math.sqrt((M[0] - P[0])**2 + (M[1] - P[1])**2) != 0 else 0.000001
+        r = math.sqrt((M[0] - P[0])**2 + (M[1] - P[1])**2)
+        r = r if r != 0 else self.accuracy
         return math.pi/2 * 1j * mpmath.hankel1(0, k * r)
 
-    def solve(self, nDielectric, nArc, dielectricRectangles, arcPoints):
-        k0=1
-        k1=2
-        c1=(k1**2-k0**2)/(2*math.pi)
-        c2=1/(2*math.pi)
-        U0S = [1.0j] * nDielectric
-        U0L = [1.0j] * nArc
+    def solve(self, nDielectric, nArc, dielectricRectangles, arcPoints, source):
         vertices = dielectricRectangles[0].getListOfVertices()
         ds = math.sqrt((vertices[0][0] - vertices[1][0])**2 + (vertices[0][1] - vertices[1][1])**2) * \
              math.sqrt((vertices[1][0] - vertices[3][0])**2 + (vertices[1][1] - vertices[3][1])**2)
         dl = math.sqrt((arcPoints[0][0]-arcPoints[1][0])**2 + (arcPoints[0][1]-arcPoints[1][1])**2)
         A = []
         B = []
+        M = []
         for i in range(nDielectric):
-            B.append(U0S[i])
+            vertices = dielectricRectangles[i].getListOfVertices()
+            M.append([vertices[1][0] - math.fabs(vertices[1][0] - vertices[2][0]) / 2,
+                 vertices[1][1] - math.fabs(vertices[1][1] - vertices[2][1]) / 2])
+            B.append(-complex(self.getInitialFieldStrength(self.e0, self.nu0, self.k0, M[i], source.point)))
         for i in range(nArc):
-            B.append(U0L[i])
+            M.append([arcPoints[i][0], arcPoints[i][1]])
+            B.append(-complex(self.getInitialFieldStrength(self.e0, self.nu0, self.k0, M[i], source.point)))
         for i in range(nDielectric+nArc):
             A0Elem = []
-            if i<nDielectric:
-                vertices = dielectricRectangles[i].getListOfVertices()
-                M = [vertices[1][0]-math.fabs(vertices[1][0]-vertices[2][0])/2,
-                     vertices[1][1]-math.fabs(vertices[1][1]-vertices[2][1])/2]
-            else:
-                M=[arcPoints[i-nDielectric][0], arcPoints[i-nDielectric][1]]
             for j in range(nDielectric+nArc):
-                I = 1 if i==j and j<nDielectric and i<nDielectric else 0
+                i1 = 1 if i==j and j<nDielectric and i<nDielectric else 0
                 if j<nDielectric:
-                    vertices = dielectricRectangles[j].getListOfVertices()
-                    P = [vertices[1][0]-math.fabs(vertices[1][0]-vertices[2][0])/2,
-                         vertices[1][1]-math.fabs(vertices[1][1]-vertices[2][1])/2]
+                    A0Elem.append(complex(self.c1*ds*(self.G(self.k0, M[i], M[j])-1/(self.c1*ds)*i1)))
                 else:
-                    P = [arcPoints[j-nDielectric][0], arcPoints[j-nDielectric][1]]
-                A0Elem.append(complex(c1*ds*(self.G(k1, M, P)-1/(c1*ds)*I) + c2*dl*self.G(k0, M, P)))
+                    A0Elem.append(complex(self.c2*dl*self.G(self.k1, M[i], M[j])))
             A.append(A0Elem)
-        # print(len(A), len(A[0]), np.array(A))
-        # print(len(B), np.array(B))
         result = np.linalg.solve(np.array(A), np.array(B))
         U = result[:nDielectric]
         PHI = result[nDielectric:]
         return [U, PHI]
 
-    def __init__(self):
-        pass
+    def __init__(self, k0, k1, I, e0, nu0):
+        self.k0 = k0
+        self.k1 = k1
+        self.c1 = (self.k1**2-self.k0**2)/(2*math.pi)
+        self.c2 = 1 / (2 * math.pi)
+        self.accuracy = 0.000001
+        self.I = I
+        self.e0 = e0
+        self.nu0 = nu0
